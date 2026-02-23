ec11bdeecb6622b400b335dc11d0f134949c1a47
Ajithto <ajith.rasasekaran@maurer-electronics.de>
2026-02-23 09:36:47 +0100

    muss

--- app.ts
+++ app.ts
@@ -1,7 +1,4 @@
-/*
- * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
+
 
 async function app () {
   const { default: validateDependencies } = await import('./lib/startup/validateDependenciesBasic')
